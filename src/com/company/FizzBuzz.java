6f16ed490a3d1b31e1609fea2b524ee51f6b681e
Łukasz <dz793t@outlook.com>
2021-04-22 22:38:04 +0200

    Second version

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -2,64 +2,44 @@ package com.company;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
 public class FizzBuzz {
-    private final List<Function<Integer, Stream<String>>> rules;
     private final int startInclusive, endExclusive;
+    private final SortedMap<Integer,String> divisorOutput;
 
-    public FizzBuzz(List<Function<Integer, Stream<String>>> rules, int startInclusive, int endExclusive) {
-        this.rules = rules;
+    public FizzBuzz(int startInclusive, int endExclusive, SortedMap<Integer, String> divisorOutput) {
         this.startInclusive = startInclusive;
         this.endExclusive = endExclusive;
+        this.divisorOutput = divisorOutput;
     }
 
     public void run(){
         IntStream.range(startInclusive, endExclusive)
                 .boxed()
-                .map(integer ->  rules.stream()
-                        .flatMap(integerStreamFunction -> integerStreamFunction.apply(integer))
-                        .findFirst()
-                        .orElseGet(() -> Integer.toString(integer))
+                .map(
+                        i -> divisorOutput.entrySet()
+                                .stream()
+                                .filter(divisorOutputEntry -> i % divisorOutputEntry.getKey() == 0)
+                                .map(Map.Entry::getValue)
+                                .reduce((s, s2) -> String.format("%s %s",s,s2))
+                                .orElseGet(() -> Integer.toString(i))
                 )
                 .forEach(System.out::println);
     }
 
-
-
     public static void main(String[] args) {
+        var divisorOutputMap = Map.of(
+                3, "Fizz",
+                5, "Buzz"
+        );
         new FizzBuzz(
-                returnRules(),
                 1,
-                101
+                101,
+                new TreeMap<>(divisorOutputMap)
         ).run();
-    }
-
-
-    private static List<Function<Integer, Stream<String>>> returnRules(){
-        List<Function<Integer, Stream<String>>> rules = new LinkedList<>();
-        rules.add(o -> {
-            if(o % 15 == 0){
-                return Stream.of("Fizz Buzz");
-            }
-            return Stream.empty();
-        });
-
-        rules.add(o -> {
-            if(o % 5 == 0){
-                return Stream.of("Buzz");
-            }
-            return Stream.empty();
-        });
-
-        rules.add(o -> {
-            if(o % 3 == 0){
-                return Stream.of("Fizz");
-            }
-            return Stream.empty();
-        });
-        return rules;
     }
 }
